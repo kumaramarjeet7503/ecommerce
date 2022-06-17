43b950dd8447cece25ac29be976350cabee8e17c
kumaramarjeet7503 <amarjeet1998@outlook.com>
2022-06-18 00:34:25 +0530

    removed product category from backend.

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -56,12 +56,6 @@ AppAsset::register($this);
                     <span>Product</span></a>
             </li>
 
-            <!-- Nav Item - Tables -->
-            <li class="nav-item">
-                <a class="nav-link" href="tables.html">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Product Category</span></a>
-            </li>
 
             <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
